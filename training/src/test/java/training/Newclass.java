618e49f560df16c6a394e5b98b6225244caf0ae9
Training_B8C.07.19 <Training_B8C.07.19@BDC8-DX-6MWHH92.dir.svc.accenture.com>
2018-07-17 12:42:22 +0530

    First Commit

---
+++ training/src/test/java/training/Newclass.java
@@ -0,0 +1,27 @@
+package training;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Newclass {
+	@Test
+	public void testLogin()
+	{
+		System.setProperty("WebDriver.chrome.driver","C:\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://github.com/login");
+		driver.findElement(By.id("login_field")).sendKeys("sanketh247");
+		driver.findElement(By.id("password")).sendKeys("gopala247");
+		driver.findElement(By.name("commit")).click();
+		String title=driver.getTitle();
+		System.out.println(title);
+		
+		Assert.assertEquals(title,"Sign in to GitHub · GitHub");
+		
+		System.out.println("Continue to test.....");
+	}
+}
